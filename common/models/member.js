d9ee137b2bdf919dd8a30e3a9f5cc634b5d45c50
Daniel Araújo <daniel.araujorn01@gmail.com>
2018-11-21 15:44:12 -0300

    created middleware

--- common/models/member.js
+++ common/models/member.js
@@ -1,5 +1,13 @@
-'use strict';
+"use strict";
 
 module.exports = function(Member) {
+  Member.remoteMethod("sayMyName", {
+    accepts: { arg: "myName", type: "string", required: true },
+    description: "Recebe um nome e retorna uma string",
+    http: { path: "/say-my-name/:myName", verb: "get" },
+    returns: { arg: "theResponse", type: "object" }
+  });
 
+  Member.sayMyName = (myName, callback) =>
+    callback(null, `Your name is ${myName}`);
 };
---
+++ server/middleware/counter.js
@@ -0,0 +1,3 @@
+module.exports = () => (req, res, next) => {
+  console.log("hello from the middleware");
+};
